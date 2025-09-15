4e4ff514856d35c3b56c8775256ce44770f6724e
Yu Zhang <ares201005@gmail.com>
2025-09-15 16:25:47 -0600

    Initial code for computing the phonon-polariton of bi-layer or bi-slab

--- PHonon/PH/polariton_mod.f90
+++ PHonon/PH/polariton_mod.f90
@@ -186,4 +186,60 @@ subroutine inv_spd(A, n, info)
   A = 0.5D0*(A + transpose(A))
 end subroutine inv_spd
 
+
+subroutine build_twisted_polaritons( &
+   nat, nmodes, amass_atom, vol_bohr3, w2, z_in, zstar, epsinf, &
+   twist_angle, thickness, distance, &
+   ncav, omega_in, omega_units, polvec, lambda_in, vmode_ang3, eps_ext, &
+   nout, wpol, evec_pol, phot_frac)
+  !---------------- arguments ----------------
+  integer, intent(in) :: nat, nmodes, ncav
+  real(DP), intent(in) :: amass_atom(nat)                       ! amu
+  real(DP), intent(in) :: vol_bohr3                             ! cell volume (bohr^3)
+  real(DP), intent(in) :: w2(nmodes)                            ! phonon squared freqs (Ry)
+  real(DP), intent(in) :: z_in(3*nat,nmodes)                    ! phonon eigenvectors (mass-weighted style)
+  real(DP), intent(in) :: zstar(nat,3,3)                        ! Born Z*
+  real(DP), intent(in) :: epsinf(3,3)                           ! epsilon_infty (ion-clamped)
+  real(DP), intent(in) :: twist_angle                           ! twist angle
+  real(DP), intent(in) :: thickness                             ! thickness of the slab ( = 1, for by-layer)
+  real(DP), intent(in) :: distance                              ! distance between the two layers (slabs)
+  real(DP), intent(in) :: omega_in(ncav)                        ! photon freq (user units)
+  character(len=*), intent(in) :: omega_units                   ! 'Ry','THz','cm-1','meV','eV'
+  real(DP), intent(in) :: polvec(3,ncav)                        ! unit polarization vectors
+  real(DP), intent(in) :: lambda_in(ncav)                       ! optional direct λ (Gaussian a.u.), <=0 -> build from Vmode
+  real(DP), intent(in) :: vmode_ang3(ncav)                      ! effective mode volumes [Å^3] (optional)
+  real(DP), intent(in) :: eps_ext                               ! background dielectric of cavity
+  integer, intent(out) :: nout
+  real(DP), intent(out) :: wpol(nmodes+ncav)                    ! polariton freqs (Ry)
+  real(DP), intent(out) :: evec_pol(nmodes+ncav,nmodes+ncav)    ! polariton eigenvectors (dimensionless)
+  real(DP), intent(out) :: phot_frac(nmodes+ncav)               ! photon fraction per mode
+  !---------------- locals ----------------
+  integer :: i, j, iat, a, b, nu, mu, al, be, ndim, info, lwork
+  real(DP) :: pi, ang2bohr, vol_ang3, convw, normp
+  real(DP) :: I3(3,3), chi(3,3), lam(3,ncav)
+  real(DP) :: wph(ncav), S(nmodes,ncav), X(ncav,ncav), IX(ncav,ncav)
+  real(DP), allocatable :: K(:,:), work(:), eval(:)
+  real(DP) :: dnu(3,nmodes), mhalf
+  !
+  pi = dacos(-1.0_DP)
+  !
+  ! ---- unit conversions (to Rydberg atomic units) ----
+  select case (trim(adjustl(omega_units)))
+  case ('Ry');   convw = 1.0_DP
+  case ('THz');  convw = 1.0_DP/RY_TO_THZ
+  case ('cm-1'); convw = 1.0_DP/RY_TO_CMM1
+  case ('meV');  convw = 1.0_DP/13.605693009D0/1.0D-3
+  case ('eV');   convw = 1.0_DP/13.605693009D0
+  case default;  convw = 1.0_DP
+  end select
+  do al=1,ncav
+     wph(al) = omega_in(al)*convw
+  enddo
+
+  ! compute the phonon-polariton of twisted bi-layer (or by-slab)
+  ! TBA
+
+
+end subroutine build_twisted_polaritons
+
 end module polariton_mod
