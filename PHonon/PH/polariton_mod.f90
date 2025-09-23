a03238ed73d5b7a2a1990baf93955b341861ca16
Yu Zhang <ares201005@gmail.com>
2025-09-23 13:38:48 -0600

    Add the construct of dynmat kernel and initial code for twisted phonon polaritons

--- PHonon/PH/polariton_mod.f90
+++ PHonon/PH/polariton_mod.f90
@@ -187,6 +187,100 @@ subroutine inv_spd(A, n, info)
 end subroutine inv_spd
 
 
+subroutine construct_polariton_dynmat(nat, nmodes, ncav, amass_atom, w2, zstar, z_in, chi, lam, K)
+  implicit none
+  integer,  intent(in) :: nat
+  integer,  intent(in) :: nmodes
+  integer,  intent(in) :: ncav
+  real(DP), intent(in) :: amass_atom(nat)                       ! amu
+  real(DP), intent(in) :: w2(nmodes)                            ! phonon squared freqs (Ry)
+  real(DP), intent(in) :: zstar(nat,3,3)                        ! Born Z*
+  real(DP), intent(in) :: z_in(3*nat,nmodes)                    ! phonon eigenvectors (mass-weighted style)
+  real(DP), intent(in) :: chi(3, 3)
+  real(DP), intent(in) :: lam(3,ncav)
+  real(DP), intent(inout) :: K(nmodes + ncav, nmodes + ncav)
+
+  !
+  integer  :: i, j, iat, a, b, nu, mu, al, be, ndim, info, lwork
+  real(DP) :: dnu(3,nmodes), mhalf
+  real(DP) :: wph(ncav), S(nmodes,ncav), X(ncav,ncav), IX(ncav,ncav)
+
+  ndim = nmodes + ncav
+  ! ---- mode effective dipoles dν (using Z* and eigenvectors) ----
+  dnu = 0.0_DP
+  do nu=1,nmodes
+     i=0
+     do iat=1, nat
+        do a=1,3
+           i = i+1
+           mhalf = 1.0_DP/sqrt(amass_atom(iat))
+           do b=1,3
+              dnu(b,nu) = dnu(b,nu) + zstar(iat, a, b) * z_in(i,nu) * mhalf
+           enddo
+        enddo
+     enddo
+  enddo
+  !
+  ! ---- X = \lambda^T \chi \lambda  and  S = \lambda\cdot d ----
+  X = 0.0_DP
+  do al=1,ncav
+     do be=1,ncav
+        do a=1,3; do b=1,3
+           X(al,be) = X(al,be) + lam(a,al)*chi(a,b)*lam(b,be)
+        enddo; enddo
+     enddo
+  enddo
+  IX = X
+  do al=1,ncav
+    IX(al,al) = IX(al,al) + 1.0_DP
+  enddo
+  call inv_spd(IX, ncav, info)   ! (I+X)^{-1} (SPD expected)
+  if (info /= 0) stop 'polariton_mod: inversion failed'
+  !
+  S = 0.0_DP
+  do nu=1,nmodes
+     do al=1,ncav
+        S(nu,al) = lam(1,al)*dnu(1,nu) + lam(2,al)*dnu(2,nu) + lam(3,al)*dnu(3,nu)
+     enddo
+  enddo
+  !
+  ! ---- Assemble symmetric K ----
+  ! WRITE(6, *) "S = ", S
+  ! K_QQ
+  do nu=1,nmodes
+     K(nu,nu) = w2(nu)
+     do mu=1,nmodes
+        do al=1,ncav
+           do be=1,ncav
+              K(nu,mu) = K(nu,mu) + S(nu,al)*IX(al,be)*S(mu,be)
+           enddo
+        enddo
+     enddo
+  enddo
+  ! WRITE(6, *) "KQQ = ", K(1:nmodes, 1:nmodes)
+  !
+  ! TODO, currently we only considered the |0, 1> photon excitation space
+  ! make it general to arbitray truncation
+  !
+  ! K_Qq (=K_qQ^T)
+  do nu=1,nmodes
+     do al=1,ncav
+        do be=1,ncav
+           K(nu, nmodes+al) = K(nu, nmodes+al) - S(nu,be)*IX(be,al)*wph(al)
+           K(nmodes+al, nu) = K(nu, nmodes+al)
+        enddo
+     enddo
+  enddo
+  ! K_qq
+  do al=1,ncav
+     do be=1,ncav
+        K(nmodes+al, nmodes+be) = wph(al)*IX(al,be)*wph(be)
+     enddo
+  enddo
+  ! WRITE(6, *) "Kqq = ", K(nmodes+1:nmodes+1, nmodes+1:nmodes+1)
+end subroutine construct_polariton_dynmat
+
+
 subroutine build_twisted_polaritons( &
    nat, nmodes, amass_atom, vol_bohr3, w2, z_in, zstar, epsinf, &
    twist_angle, thickness, distance, &
@@ -218,7 +312,7 @@ subroutine build_twisted_polaritons( &
   real(DP) :: pi, ang2bohr, vol_ang3, convw, normp
   real(DP) :: I3(3,3), chi(3,3), lam(3,ncav)
   real(DP) :: wph(ncav), S(nmodes,ncav), X(ncav,ncav), IX(ncav,ncav)
-  real(DP), allocatable :: K(:,:), work(:), eval(:)
+  real(DP), allocatable :: K0(:, :), K(:,:), work(:), eval(:)
   real(DP) :: dnu(3,nmodes), mhalf
   !
   pi = dacos(-1.0_DP)
@@ -238,6 +332,71 @@ subroutine build_twisted_polaritons( &
 
   ! compute the phonon-polariton of twisted bi-layer (or by-slab)
   ! TBA
+  ! ---- χ from ε∞ : χ = (ε∞ - I) * Ω_cell / (4π)  (Gaussian a.u.) ----
+  I3 = 0.0_DP; do a=1,3; I3(a,a)=1.0_DP; enddo
+  chi = (epsinf - I3) * (vol_bohr3/(4.0_DP*pi))
+  !
+  ! ---- cavity couplings λ ----
+  ang2bohr = 1.0_DP/BOHR_RADIUS_ANGS
+  do al=1,ncav
+     normp = max(1.0D-14, sqrt(polvec(1,al)**2+polvec(2,al)**2+polvec(3,al)**2))
+     if (lambda_in(al) > 0.0_DP) then
+        lam(:,al) = polvec(:,al)/normp * lambda_in(al)
+     else if (vmode_ang3(al) > 0.0_DP) then
+        ! λ = ê * sqrt(4π / (Veff * ε_ext)), with Veff in bohr^3
+        vol_ang3 = max(1.0D-24, vmode_ang3(al))
+        lam(:,al) = polvec(:,al)/normp * sqrt( 4.0_DP*pi / ( (vol_ang3*(ang2bohr**3)) * max(eps_ext,1.0D-12) ) )
+     else
+        ! if both lamda and voluem are zero, set lam zero
+        lam(:,al) = 0.0_DP
+     endif
+  enddo
+  ! WRITE(6, *) 'DEBUG-YZ: lam = ', lam
+  !
+  ndim = 2 * nmodes + ncav
+  allocate(K0(nmodes+ncav, nmodes+ncav))
+  K0 = 0.0_DP
+  call construct_polariton_dynmat(nat, nmodes, ncav, amass_atom, w2, zstar, z_in, chi, lam, K0)
+  !
+  allocate(K(2*nmodes + ncav, 2*nmodes + ncav))
+
+  ! 1) first layer/slab
+  K(1:nmodes, 1:nmodes) = K0(1:nmodes, 1:nmodes)
+
+  ! 2) rotate second K for the second layer/slab
+  ! K(nmodes + 1: 2*nmodes, nmodes+1:2*nmodes) =
+
+  ! 3) add the coupling between two-layers/slabs
+  ! K(1:nmodes, nmodes+1:2*nmodes) = xx
+
+  ! 4) coupling between each layer/slab and cavity photon
+  ! K(1:nmodes, 2*nmodes + ncav) = xx
+  ! K(2*nmodes + ncav, 1:nmodes) = xx
+  ! K(1+nmodes:2*nmodes, 2*nmodes + ncav) = xx
+  ! K(2*nmodes + ncav, 1+nmodes:2*nmodes) = xx
+
+
+  ! ---- Diagonalize K (symmetric) ----
+  allocate(eval(ndim))
+  lwork = max(1, 3*ndim)
+  allocate(work(lwork))
+  call dsyev('V','U',ndim,K,ndim,eval,work,lwork,info)
+  if (info /= 0) stop 'polariton_mod: diagonalization failed'
+  !
+  nout = ndim
+  do i=1,ndim
+     wpol(i) = sqrt(max(0.0_DP, eval(i)))
+  enddo
+  evec_pol(:,:) = K(:,:)     ! eigenvectors returned in K
+  !
+  phot_frac = 0.0_DP
+  do i=1,ndim
+     do al=1,ncav
+        phot_frac(i) = phot_frac(i) + evec_pol(nmodes+al,i)**2
+     enddo
+  enddo
+  !
+  deallocate(K, eval, work)
 
 
 end subroutine build_twisted_polaritons
